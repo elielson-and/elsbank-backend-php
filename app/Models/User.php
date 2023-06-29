69673abc656786cafb32a0e5291311ed3ca095ce
Elielson André <68979955+elielson-andre@users.noreply.github.com>
2023-06-28 23:34:12 -0300

    fix: adding uuid to be fillable and hide id when show json

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,6 +18,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $fillable = [
+        'uuid',
         'first_name',
         'last_name',
         'email',
@@ -30,6 +31,7 @@ class User extends Authenticatable
      * @var array<int, string>
      */
     protected $hidden = [
+        'id',
         'password',
         'remember_token',
     ];
