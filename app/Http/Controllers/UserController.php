194a53de56f44a3da82c3606b7ea6b4d4ec72258
Elielson André <68979955+elielson-andre@users.noreply.github.com>
2023-07-02 02:36:44 -0300

    refactor: deleting some unused methods

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -19,26 +19,6 @@ class UserController extends Controller
     ];
 
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
@@ -105,17 +85,6 @@ class UserController extends Controller
         }else{
             return response()->json(['message'=>'payee not found'],404);
         }
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
     }
 
     /**
